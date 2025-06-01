3deb63c6b0a75b50931f8e7cbcefcc500f9537fe
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-02 01:35:23 +0530

    script.js2

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,36 @@
+//***** Activating strict mode *****//
+
+let hasDriveringLicence = false;
+const passTest = true;
+
+if(passTest) hasDriveringLicence = true ;
+if (hasDrivingLicence) console.log('I can drive :D');
+
+//const interface = 'Audio';
+//const private = 534;
+
+
+//****Functions  ****//
+function logger(){
+    console.log('my name is shanaya');
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples,oranges) {
+    console.log(apples , oranges);
+    const juice = 'Juice with ${apples} apples and ${oranges}' oranges;
+    return juice ;
+}
+
+const applejuice = fruitProcessor(5,0);
+console.log(applejuive.juice);
+
+const appleOrange = fruitProcessor(10,5);
+console.log(appleOrange.juice);
+
+
+//**** Functions Declearation ****//
