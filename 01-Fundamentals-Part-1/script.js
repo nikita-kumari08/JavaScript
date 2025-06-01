68aa0a37f705561d024463daa419aadf349ca025
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-01 23:59:10 +0530

    script.js1

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -350,6 +350,32 @@ if(hasDriversLicence && hasGoodVision){
                                     console.log('Great language too :D');
     }
     
+
+
+    const day = 'monday';
+
+    switch (day)
+    case 'monday': // day === 'monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+    case 'tuesday' :
+        console.log('Prepare theory videos');
+        break ;
+    case 'wednesday':
+    case 'thursday' :
+        console.log('Writes code examples');
+        break ;
+    case 'friday' :
+        console.log('Record videos ');
+        break ;
+        case 'saturday' ;
+        case 'sunday' :
+            console.log('Enjoy the weekend :D');
+            break;
+         default:
+            console.log('Not a valid day!');
+}
     
     //**** The conditional ****//
 
