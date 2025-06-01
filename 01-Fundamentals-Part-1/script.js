810f9d7d386180fb8af95ea96c2db61366d267fc
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-01 18:51:26 +0530

    script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,2 +1,39 @@
 let myNumber = 55;
 console.log(myNumber);
+
+let country = 'Protugal' ;
+let continent = 'Europe' ;
+let population = 10;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof language);
+
+language = 'portugues';
+const country = 'portugal';
+const isIsland = false ;
+isIsland true ;
+
+console.log(population / 2);
+population ++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+count description =
+country +
+' is in' +
+continent +
+',and its ' +
+population +
+'milion people speak ' +
+language;
+console.log(description1);
+
+
+
+
