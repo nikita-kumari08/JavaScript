9e6eb9980863c440b04cc088aae56bdaac1aa056
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-02 21:24:48 +0530

    script.js2

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -11,3 +11,35 @@ const calcAge = birthYear => 2037 - birthYear ;
 
 
 console.log(calcAge(1991));
+
+
+//*** Problem - 1 ***//
+
+
+const calcTempAmplitude = function (temps) {
+let max = temps[0];
+let min = temps[0];
+
+for(let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue ;
+
+    if(curTemp > max) max = curTemp;
+    if(curTemp < min) min = curTemp;
+}
+console.log(max,min);
+return max - min;
+};
+
+const amplitude = calcTempAmplitude{temperature};
+console.log(amplitude);
+
+
+
+//*** Problem - 2 ***//
+
+
+
+
+
+
