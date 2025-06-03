1473cff840bedfe9e6ae750cc442b2fb8327f496
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-03 19:01:01 +0530

    script.js6

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -3,8 +3,34 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close - modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+const btnsOpenModal = document.querySelectorAll('open - modal');
+
+
+const openModal = function(){
+console.log('Button clicked');
+modal.classList.remove('hidden');
+overlay.classList.remove('hidden')
+};
+
+
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+
 
 for(let i = 0; i < btnsOpenModal.length; i++)
-console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener('click',function
+ (){
+ c;
+
+modal.style.display = 'block';
+
+});
+
+
+
+btnCloseModal.addEventListener('click' , closeModal ()); 
+overlay.addEventListener('click' , closeModal);
