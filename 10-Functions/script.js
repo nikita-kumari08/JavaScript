efb5bb4d505dfb5d3a97b12a1d74ea6ea15c4b7c
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-04 20:22:42 +0530

    script.js10

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const bookings = [];
+
+const createBoking = function(flightNum,
+    numPassenger = 1 , price = 199){
+        //ES5
+        //numPassengers = numPassenger || 1;
+        // price = price || 199;
+
+    const bookings = {
+        flightNum,
+        numPassengers,
+        price,
+    };
+    console.log(booking);
+    bookings.push(booking);
+
+};
+
+createBooking|('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+
+createBooking('LH123', undefined, 1000);
