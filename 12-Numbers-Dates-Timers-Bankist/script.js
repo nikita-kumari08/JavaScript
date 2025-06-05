457f6c241a383b5b42ec52db9a316d277877dee7
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-06 02:18:30 +0530

    script.js12

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -456,3 +456,28 @@ console.log(new Date(2037, 10, 31));
 console.log(new Date(0));
 console.log(new Date(3 * 24 * 60 * 60 * 1000));
 
+
+
+
+//****  Working with dates  ****//
+
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142256980000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+
