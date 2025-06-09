7e4acaf6d5b4a04bbecd1fbe44087bf18a29f4a8
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-09 19:09:09 +0530

    script.js15

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -235,7 +235,7 @@ navigator.getlocation.getCurrentPosition (this.loapMap.bind(this),function () {
 
  _renderWorkout(workout){
 
-const html = `
+let html = `
 <li class="workout workout--${workout.type}" data-id="${workout.id}">
           <h2 class="workout__title">${workout.description}</h2>
           <div class="workout__details">
@@ -250,5 +250,36 @@ const html = `
             <span class="workout__value">${workout.duration}</span>
             <span class="workout__unit">min</span>
           </div>
-          `
- }
+          `;
+
+          if(workout.type === 'running')
+            html += `
+        <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.pace.toFixed(1)}</span>
+            <span class="workout__unit">min/km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">🦶🏼</span>
+            <span class="workout__value">${workout.cadence}</span>
+            <span class="workout__unit">spm</span>
+          </div>
+        </li>
+        `;
+
+        if(workout.type === 'running')
+            html += `
+
+            <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">workout.speed</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">workout.elevationGain</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li> -->
+        `;
+}
