4064ed69358f8ab54527a124b43c1ac878774512
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-10 16:11:43 +0530

    script.js16

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -13,11 +13,13 @@ const countriesContainer = document.querySelector('.countries');
 
 const request = new XMLHttpRequest();
 request.open('GET','https://restcountries.eu/rest/v2/name/india');
-data = request.send();
+request.send();
+
 
 
 request.addEventListener('load', function() { 
-    console.log(this.responseText);
+ const data = JSON.parse(this.responseText);
+ console.log(data);
 })
 
 
