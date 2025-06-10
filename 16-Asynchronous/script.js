dee22ad78932cff5cee4cf0ec19b3376f66f067f
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-10 17:30:59 +0530

    script.js16

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -560,3 +560,27 @@ const wait = function (seconds) {
  -621,26 +596,6 
 
 let currentImg;
+
+//createImage('img/img-1.jpg')Add comment
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 1 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 2 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//   })
+//   .catch(err => console.error(err));
+
+// PART 1
+const loadNPause = async function () {
+  try {
