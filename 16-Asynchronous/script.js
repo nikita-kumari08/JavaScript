9bf2b753a7d130713c9d14a0d949ad8db11bf407
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-10 17:20:04 +0530

    script.js16

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -231,6 +231,84 @@ btn.addEventListener('click', function () {
 
    // Challenge - 1
 
+   const whereAmI = function (lat, lng) {
+  fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`)
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.countryCode}`);
+
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message} 💥`));
+};
+
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
+whereAmI(52.508, 13.381);
 
 
 
+// The Event Loop 
+
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
+
+
+
+// Building Promise
+
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lotter draw is happening ');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN ');
+    } else {
+      reject(new Error('You lost your money '));
+    }
+  }, 2000);
+});
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+wait(1)
+  .then(() => {
+    console.log('3 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 second passed'));
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.error(x));
