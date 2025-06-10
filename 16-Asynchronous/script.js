ed8c974d4285d896292e341e574b9022dcbb8cd9
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-10 18:48:58 +0530

    script.js16

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -525,7 +525,7 @@ getPosition = function () {
 
 whereAmI = async function () {
   try {
-    
+
     // Geolocation
 
     const pos = await getPosition();
@@ -567,10 +567,21 @@ console.log('1: Will get location');
 //   .finally(() => console.log('3: Finished getting location'));
 
 
-(async function ()try { -512,20 +505,9 })();
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(`2: ${city}`);
+  } catch (err) {
+    console.error(`2: ${err.message} 💥`);
+  }
+  console.log('3: Finished getting location');
+})();
+
 
 
 // Running Promises in Parallel
+
+
 const get3Countries = async function (c1, c2, c3) 
   try {
     // const [data1] = await getJSON(
@@ -584,25 +595,79 @@ const get3Countries = async function (c1, c2, c3)
     // );
     // console.log([data1.capital, data2.capital, data3.capital]);
 
-    const data = await Promise.all
+    const data = await Promise.all([
       getJSON(`https://restcountries.com/v2/name/${c1}`),
- -539,10 +521,6 
-};
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  };
+ 
 get3Countries('portugal', 'canada', 'tanzania');
 
 
 
-// Other Promise Combinators: race, allSettled and any
+// Other Promise Combinators: race, allSettled and anyMore actions
+
 
 // Promise.race
 
 (async function () {
   const res = await Promise.race([
     getJSON(`https://restcountries.com/v2/name/italy`),
- -591,9 +569,6 
+    getJSON(`https://restcountries.com/v2/name/egypt`),
+    getJSON(`https://restcountries.com/v2/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took too long!'));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.com/v2/name/tanzania`),
+  timeout(5),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+
+// Promise.allSettled
+
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
   .then(res => console.log(res))
   .catch(err => console.error(err));
-  ])};
+
+
+
+// Promise.any [ES2021]
+
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+
 
 // challenge - 3
 
