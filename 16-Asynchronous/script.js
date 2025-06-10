fd204b56db4fb673d3e5f8e5af8da0e6a90f5ed8
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-10 17:15:54 +0530

    script.js16

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -11,6 +11,8 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+
+const getCountryData = function(country) {
 const request = new XMLHttpRequest();
 request.open('GET','https://restcountries.eu/rest/v2/name/india');
 request.send();
@@ -47,6 +49,46 @@ request.addEventListener('load', function() {
             html);
             countriesContainer.style.opacity = 1;
        });
+    };
+
+// XMLHttpRequest
+
+const getCountryData = function (country) {
+
+  request.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+
+
+
+
+    getCountryData('india');
+    getCountryData('usa');
+    getCountryData('china');
 
 
 
