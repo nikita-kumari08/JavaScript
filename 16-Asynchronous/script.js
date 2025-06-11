ec96136cd3b503786c612a692cbada555cef84dc
Nikita Kumari <nikitarai7305@gmail.com>
2025-06-11 14:50:38 +0530

    script.js16

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -304,6 +304,20 @@
 //   })
 //   .then(() => console.log('4 second passed'));
 
+// setTimeout(() => {More actions
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+
 // Promise.resolve('abc').then(x => console.log(x));
 // Promise.reject(new Error('Problem!')).catch(x => console.error(x));
 
